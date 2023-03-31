9f3790d398d1e567959c29adc137848043e013e7
nguyenbaongoc123 <127759913+nguyenbaongoc123@users.noreply.github.com>
2023-03-31 21:29:31 +0900

    Create ktchanle.py

---
+++ ktchanle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+n= int(input())
+if n%2==0:
+    print("la so chan")
+elif n%2!=0:
+    print('la so le')
+else:
+    print("khong phai la so tu nhien")
+
+
+# In[ ]:
+
+
+
+
